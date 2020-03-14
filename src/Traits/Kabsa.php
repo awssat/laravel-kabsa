ea97aad014a1f10d7eb2146ce13d9221a3339205
bader almutairi <phpfalcon@gmail.com>
2020-03-15 02:04:37 +0300

    Update Kabsa.php

--- src/Traits/Kabsa.php
+++ src/Traits/Kabsa.php
@@ -7,11 +7,17 @@ use Illuminate\Database\Eloquent\Collection;
 
 trait Kabsa
 {
+    public function getRows()
+    {
+        return $this->rows;
+    }
+
     public static function all($columns = [])
     {
         self::unguard();
         $self = new self();
-        return Collection::make($self->rows ?? [])->map(function ($row) { return new self($row); });
+
+        return Collection::make($self->getRows() ?? [])->map(function ($row) { return new self($row); });
     }
 
     public function __call($method, $parameters)
